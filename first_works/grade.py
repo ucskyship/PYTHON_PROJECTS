44072a97de9bcfd2aa3484ab02bd760c6665ab07
UZOCHUKWU CHINEMEREM <chat.with.ucjhay@gmail.com>
2022-06-02 12:04:21 +0100

    My first python work

--- first_works/grade.py
+++ first_works/grade.py
@@ -1,3 +1,14 @@
-grade = input("Enter your score for grade")
+grade = int(input("Enter your score for grade: "))
 user_input = grade
-if (user_input
+if 70 <= grade <= 100:
+    print("Your grade is 'A'")
+elif 60 <= grade < 70:
+    print("Your grade is 'B'")
+elif 50 <= grade < 60:
+    print("Your grade is 'C'")
+elif 45 <= grade < 50:
+    print("Your grade is 'D'")
+elif 40 <= grade < 45:
+    print("Your grade is 'E'")
+elif grade < 40:
+    print("Your grade is 'F'")
