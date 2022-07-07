9f92cfcc060bf61eb7774acb835ba1fc155f05e5
UZOCHUKWU CHINEMEREM <chat.with.ucjhay@gmail.com>
2022-07-07 10:11:58 +0100

    initial commit update

--- module_/Mod1.py
+++ module_/Mod1.py
@@ -1,5 +1,14 @@
 print(__name__)
 
+
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
 if __name__ == '__main__':
     print("I am invisible to other modules")
     print("Mod1", __name__)
--- module_/Mod2.py
+++ module_/Mod2.py
@@ -1,4 +1,10 @@
-import Mod1
+# import Mod1
+# import Mod1 as a
+from Mod1 import add as af, sub as fg
+# __add__ = ['add'] this is used when someone wants to import everything he as on a module and wants to restrict some
+#  functions not to import you can the __all__ = ['ajsd'] this now restrict other functions from been imported
+# and all the specified ones in the square brackets will be imported
 
-print(__name__)
-print(Mod1.__name__)
+
+# print(__name__)
+# print(Mod1.__name__)
